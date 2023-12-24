9796731c831a0bcde7dc37c3e8a7c08ad6c11897
JinTaekLim <jintaek019@gmail.com>
2023-12-24 17:48:20 +0900

    181896

---
+++ src/Lv0/Q_181896.java
@@ -0,0 +1,18 @@
+package Lv0;
+
+public class Q_181896 {
+    public static class Solution {
+        public int solution(int[] num_list) {
+            int answer = 0;
+
+            for(int i=0; i<num_list.length; i++){
+                if(num_list[i] < 0 ) {
+                    answer = i;
+                    break;
+                }
+                else answer = -1;
+            }
+            return answer;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import Lv0.Q_181897;
+import Lv0.Q_181896;
 
 import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) {
-        Q_181897.Solution Su = new Q_181897.Solution();
+        Q_181896.Solution Su = new Q_181896.Solution();
 
         //System.out.println(Arrays.toString(Su);
         //System.out.println(Su);
@@ -13,14 +13,16 @@ public class Main {
         int[] A1 = {1, 5, 2};
         int[] A2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        int[] B1 = {0,1,1};
+        int[] B1 = {12, 4, 15, 46, 38, -2, 15};
         int[] B2 = {10,8,6,4,2};
 
 
 
 
 
-        System.out.println(Arrays.toString(Su.solution(2,B1,B2)));
+        //System.out.println(Arrays.toString(Su.solution(A1)));
+        System.out.println(Su.solution(B1)); // A1 배열을 전달해서 결과 출력
+
         //System.out.println(Arrays.toString(Su.solution(3,B1,B2)));
 
     }
